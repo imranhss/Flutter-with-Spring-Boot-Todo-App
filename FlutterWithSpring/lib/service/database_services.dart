689749fae38198830086c261f63f90605b6dca47
Muhammad Emran Hossain <emranhss@gmail.com>
2023-11-22 09:32:07 +0600

    add

--- FlutterWithSpring/lib/service/database_services.dart
+++ FlutterWithSpring/lib/service/database_services.dart
@@ -7,6 +7,7 @@ import 'package:http/http.dart' as http;
 class DatabaseServices {
 
   static final String baseURL = "http://localhost:8082";
+
   static final  Map<String, String> headers = {"Content-Type": "application/json"};
 
   //for add data into DB
--- SpringWithFlutter/src/main/java/com/emranhss/SpringWithFlutter/restcontroller/TaskItemController.java
+++ SpringWithFlutter/src/main/java/com/emranhss/SpringWithFlutter/restcontroller/TaskItemController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@CrossOrigin("*")
+
 public class TaskItemController {
 
     @Autowired
